380008867d76ec201771e0528786622ae77679f5
zengchen1024 <chenzeng2@huawei.com>
2023-01-10 10:29:51 +0800

    optimize the kafka init

--- config.go
+++ config.go
@@ -2,41 +2,51 @@ package main
 
 import (
 	"errors"
+	"strings"
+
+	"github.com/opensourceways/community-robot-lib/kafka"
+	"github.com/opensourceways/community-robot-lib/mq"
 )
 
 type configuration struct {
-	Config accessConfig `json:"access,omitempty"`
-}
-
-func (c *configuration) Validate() error {
-	return c.Config.validate()
-}
-
-func (c *configuration) SetDefault() {}
-
-type accessConfig struct {
-	Topic          string `json:"topic" required:"true"`
-	UserAgent      string `json:"user_agent" required:"true"`
+	Topic          string `json:"topic"           required:"true"`
+	UserAgent      string `json:"user_agent"      required:"true"`
+	KafkaAddress   string `json:"kafka_address"   required:"true"`
 	AccessEndpoint string `json:"access_endpoint" required:"true"`
 	ConcurrentSize int    `json:"concurrent_size" required:"true"`
 }
 
-func (a *accessConfig) validate() error {
-	if a.Topic == "" {
+func (c *configuration) Validate() error {
+	if c.Topic == "" {
 		return errors.New("missing topic")
 	}
 
-	if a.UserAgent == "" {
+	if c.UserAgent == "" {
 		return errors.New("missing user_agent")
 	}
 
-	if a.AccessEndpoint == "" {
+	if c.KafkaAddress == "" {
+		return errors.New("missing kafka_address")
+	}
+
+	if c.AccessEndpoint == "" {
 		return errors.New("missing access_endpoint")
 	}
 
-	if a.ConcurrentSize <= 0 {
+	if c.ConcurrentSize <= 0 {
 		return errors.New("Concurrent_size must be > 0")
 	}
 
 	return nil
+}
+
+func (c *configuration) SetDefault() {}
+
+func (c *configuration) kafkaConfig() (cfg mq.MQConfig, err error) {
+	v := strings.Split(c.KafkaAddress, ",")
+	if err = kafka.ValidateConnectingAddress(v); err == nil {
+		cfg.Addresses = v
+	}
+
+	return
 }
